a43a60ae4551889eabaa8f1fe0f780b3c43bc178
foliwe <foliwe@gmail.com>
2020-01-30 22:51:56 +0000

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get '/check.txt', to: proc {[200, {}, ['it_works']]}
+
+  root to: 'home#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
